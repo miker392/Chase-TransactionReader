86ab59d23616f4ac3069010c2eca4c3771519be4
Mike Rooney <miker392@users.noreply.github.com>
2023-04-17 20:52:22 -0500

    Small fix to get rid of the $ sign as well

--- Program.cs
+++ Program.cs
@@ -19,7 +19,9 @@ namespace TextFileParsingExample
                     lines = Array.FindAll(lines, line => !line.Contains(phraseToRemove));
                 }
 
-                lines = lines.Select(line => line.Replace($", {currentYear}", $" {currentYear}")).ToArray();
+                lines = lines.Select(line => line
+                                            .Replace($", {currentYear}", $" {currentYear}")
+                                            .Replace("$", "")).ToArray();
 
                 // Remove empty lines
                 lines = Array.FindAll(lines, line => line.Trim().Length > 0);
